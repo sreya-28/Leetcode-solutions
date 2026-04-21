1ef44b3561d9ab0f183239a3cd97bf86dfc6089a
sreya-28 <sreyamerinsam2828@gmail.com>
2026-04-21 10:47:21 +0530

    Add two-sum solution in two-sum-2.py
    Implement two-sum solution for sorted array.

---
+++ python/two-sum-2.py
@@ -0,0 +1,21 @@
+#two-sum-ii-input-array-is-sorted
+#difficulty: medium
+#language: python
+#link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=0
+        r=len(numbers)-1
+        while l<r :
+            s=numbers[l]+numbers[r]
+            if s==target:
+                return [l+1,r+1]
+            elif s<target:
+                l+=1
+            else:
+                r-=1
