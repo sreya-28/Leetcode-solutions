58fc64968ae98cde00f81e209903c8959cc59f91
sreya-28 <sreyamerinsam2828@gmail.com>
2026-04-21 11:43:37 +0530

    Implement setZeroes function for matrix modification

---
+++ python/set-matrix-zeroes.py
@@ -0,0 +1,23 @@
+#set matrix zeroes
+#difficulty: medium
+#language: python
+#link: https://leetcode.com/problems/set-matrix-zeroes/
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        r=len(matrix)
+        c=len(matrix[0])
+        rows=set()
+        cols=set()
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j]==0:
+                    rows.add(i)
+                    cols.add(j)
+        print(rows,cols)
+        for i in range(r):
+            for j in range(c):
+                if i in rows or j in cols:
+                    matrix[i][j]=0
